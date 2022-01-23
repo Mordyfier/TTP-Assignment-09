38e64f1d0d9211a696aa300fa52306ce635d83ae
AdaMaldonado <34352705+AdaMaldonado@users.noreply.github.com>
2022-01-22 23:37:10 -0500

    add some styling to bank interface

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,6 +3,12 @@ import React from 'react';
 export default function Transaction(props) {
     const transactionDate = new Date(props.transaction.date);
     return (
-        <h1>{props.transaction.description} ${props.debit && '-'}{props.transaction.amount} {transactionDate.toLocaleTimeString()} {transactionDate.toLocaleDateString()}</h1>
+        <div className='transactions'> 
+                <td className='desc'>{props.transaction.description}</td> 
+                <td className='symbol'>${props.debit}</td>
+                <td className='amount'>{props.transaction.amount}</td> 
+                <td className='time'>{transactionDate.toLocaleTimeString()}</td> 
+                <td className='day'>{transactionDate.toLocaleDateString()}</td>
+        </div>
     )
 }
